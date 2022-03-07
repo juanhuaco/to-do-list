bfce1f1d418d49bad5073f87c73eb2cb55367507
juanhuaco <juanpablohuacodelcarpio@gmail.com>
2022-03-07 20:40:48 -0300

    agregada la funcion filter pero solo imprime las rows jeje

--- js/view.js
+++ js/view.js
@@ -1,5 +1,6 @@
 import Modal from './components/modal.js';
 import TD from './components/add-todo.js';
+import Filters from './components/filters.js'
 
 export default class View {
     constructor(){
@@ -8,13 +9,25 @@ export default class View {
         this.modal = new Modal();
         this.table = document.getElementById('table');
         this.addToDoForm = new TD();
+        this.filters = new Filters();
+
         this.addToDoForm.onClick((title, description) => this.addToDo(title, description));
         this.modal.onClick((id, values)=>this.editToDo(id, values));
+        this.filters.onClick((filters)=>this.filter(filters));
+
     }
 
     render(){
         const toDos = this.model.toDos;
         toDos.forEach((toDo) => this.createRow(toDo));
+    }
+
+    filter(filters){
+        const { type, words } = filters;
+        const [, ...rows] = document.getElementsByTagName('tr');
+        for(const row of rows){
+            console.log(row);
+        }
     }
 
     setModel(model){
